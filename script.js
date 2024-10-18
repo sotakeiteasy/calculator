62b861c2bf7072211d69613cd32afaa8bf4b43cd
sotakeiteasy <murasov96@gmail.com>
2024-10-18 12:57:01 +0300

    fix legnth of result

--- script.js
+++ script.js
@@ -2,33 +2,20 @@ const btnClear = document.querySelector('#clearButton');
 const btnResult = document.querySelector('#resultButton');
 const btnNumber = document.querySelectorAll('.number');
 const btnOperator = document.querySelectorAll('.operator');
-
 const display = document.querySelector('#display');
-
-
   
 function operate(a, oper, b) {
-    a = parseInt(a)
-    b = parseInt(b)
-
-    console.log('operate')
+    a = parseFloat(a)
+    b = parseFloat(b)
       if (oper === '+') a += b;
       if (oper === '-') a -= b;
       if (oper === '*') a *= b;
       if (oper === '/') a /= b;
-    result = Math.round(a * 10000000) / 10000000;
-    console.log(result)
+    result = Math.round(a * 100000) / 100000;
     return Math.min(result, 999999999);
 }
 
-console.log(btnNumber);
-
-let displayContent = '';
-
-let numberOne = ''; 
-let numberTwo = ''; 
-let operator = '';
-
+let displayContent = ''; numberOne = ''; numberTwo = ''; operator = '';
 
 btnNumber.forEach(btn => {
     btn.addEventListener('click', (event) => {
@@ -38,7 +25,7 @@ btnNumber.forEach(btn => {
                 numberOne = input;
                 displayContent = input;
                 display.textContent = displayContent;
-            }else if(displayContent.length >= 9 || displayContent.length == undefined){
+            }else if(displayContent.length >= 9){
 
             }else{
                 numberOne += input;
@@ -46,15 +33,15 @@ btnNumber.forEach(btn => {
                 display.textContent = displayContent;
             }
         }else {
-            if(display.textContent === '0'){
+            if(displayContent === '0'){
                 numberTwo = input;
                 displayContent = input;
                 display.textContent = displayContent;
-            }else if(displayContent.length >= 9 || displayContent.length == undefined){
+            }else if(displayContent.length >= 9){
 
             }else{
                 numberTwo += input;
-                displayContent += input;
+                displayContent = numberTwo;
                 display.textContent = displayContent;
             }
         }
@@ -63,19 +50,19 @@ btnNumber.forEach(btn => {
 
 btnOperator.forEach(btn => {
     btn.addEventListener('click', (event) => {
+
+
         const input = event.target.textContent;
         if(numberTwo === '' && operator === '') {
             operator = input;
             displayContent = '';
             display.textContent += displayContent;
-
         }else if(numberTwo !== '' && operator !== ''){
             displayContent = operate(numberOne, operator, numberTwo);
             display.textContent = displayContent;
             numberOne = displayContent;
-            operator = ''
+            operator = input
             numberTwo = ''
-
         }else if(numberTwo === '' && operator !== ''){
             operator = input;
 
@@ -95,6 +82,18 @@ btnResult.addEventListener('click', () => {
             display.textContent = displayContent
         }else{
             displayContent = operate(numberOne, operator, numberTwo);
+
+            const numStr = displayContent.toString();
+            const [integerPart] = numStr.split('.');
+
+            // Calculate the number of decimal places allowed
+            const decimalPlaces = Math.max(0, 9 - integerPart.length);
+
+            // Round the number to the allowed number of decimal places
+            const roundedNumber = displayContent.toFixed(decimalPlaces);
+
+            displayContent = parseFloat(roundedNumber).toString(); // Remove any trailing zeros
+
             display.textContent = displayContent;
             numberOne = displayContent;
             operator = ''
